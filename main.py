b9eeb0b9533103631e7ad87a3996b23e055436a2
kust <vad.velikanov@yandex.ru>
2024-11-15 23:38:53 +0300

    login page fixed

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 app.config['TEMPLATES_AUTO_RELOAD'] = True  # Remove in final version
 app.jinja_env.auto_reload = True  # Remove in final version
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
 
 if __name__ == '__main__':
