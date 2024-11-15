12491b82930d501be903a3fa54266aad9b4b8d70
Ted <58144503+BUSH222@users.noreply.github.com>
2024-11-15 18:39:49 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True  # Remove in final version
+app.jinja_env.auto_reload = True  # Remove in final version
+
+
 if __name__ == '__main__':
-    pass  # Do something
+    app.run(host='localhost', port=5000, debug=True)
