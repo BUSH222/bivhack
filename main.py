f9c2a200f972c3965b00cd35c4e46e21eebca329
kust <vad.velikanov@yandex.ru>
2024-11-16 15:44:21 +0300

    main page fixed

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True  # Remove in final version
 app.jinja_env.auto_reload = True  # Remove in final version
 
+@app.route('/')
+def main():
+    # List of insurance types to pass to the template
+    insurance_types = ["Краткосрочное", "Долгосрочное", "Полное", "Минимальное"]
+    return render_template("main.html", insurance_types=insurance_types)
+
 @app.route('/login')
 def login():
     return render_template("login.html")
@@ -12,10 +18,6 @@ def login():
 @app.route('/register')
 def register():
     return render_template("register.html")
-
-@app.route('/main_page')
-def main():
-    return render_template("main.html")
 
 @app.route('/admin')
 def admin():
